571447736e059fb446508aac3e652d7ce8e0202c
Your Name <you@example.com>
2021-04-21 14:01:44 +0600

    Added new files home work code

--- src/components/CounterSimpleControls.js
+++ src/components/CounterSimpleControls.js
@@ -1,19 +1,54 @@
 import { useDispatch } from "react-redux";
 
+
 const CounterSimpleControls = () => {
+
   const dispatch = useDispatch();
+
   function incrementBy1Callback() {
     dispatch({ type: "increment_by_1" });
   }
+  
   function decrementBy1Callback() {
-    dispatch({ type: "increment_by_1" });
+    dispatch({ type: "decrement_by_1" });
   }
+
+  function incrementBy10Callback() {
+    dispatch({ type: "increment_by_10" });
+  }
+  
+  function decrementBy10Callback() {
+    dispatch({ type: "decrement_by_10" });
+  }
+
+  function multiplyBy2Callback() {
+    dispatch({ type: "multiply_by_2" });
+  }
+  
+  function divideBy2Callback() {
+    dispatch({ type: "divide_by_2" });
+  }
+  
+  // function randomCallback(max) {
+  //   dispatch({ type: "random" });
+  // }
+  function canouttoCallback(max) {
+    dispatch({ type: "canoutto" });
+  }
+    
+  
+
   return (
     <div>
       <button onClick={incrementBy1Callback}>+1</button>
       <button onClick={decrementBy1Callback}>-1</button>
-     
+      <button onClick={incrementBy10Callback}>+10</button>
+      <button onClick={decrementBy10Callback}>-10</button>
+      <button onClick={multiplyBy2Callback}>*2</button>
+      <button onClick={divideBy2Callback}>/2</button>
+      {/* <button onClick={randomCallback}>*9</button> */}
+      <button onClick={canouttoCallback}>0</button>
     </div>
   );
-}
+};
 export default CounterSimpleControls;
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,11 +1,33 @@
+import React, { Component } from 'react';
+
 const initialState = {
-    number: 0,
-  };
-  const Reducers = (state = initialState, action) => {
-    switch (action.type) {
+  number: 0,
+}
+
+const counterReducers = (state = initialState, action) => {
+  switch (action.type) {
       case "increment_by_1":
-        return { ...state, number: state.number + 1 };
-    }
-    return state;
+          return { ...state, number: state.number + 1 }
+      case "decrement_by_1":
+          return { ...state, number: state.number - 1 }
+      case "increment_by_10":
+          return { ...state, number: state.number + 10 }
+      case "decrement_by_10":
+          return { ...state, number: state.number - 10 }
+      case "multiply_by_2":
+          return { ...state, number: state.number * 2 }
+      case "divide_by_2":
+          return { ...state, number: state.number / 2 }
+    //   case "random":
+    //     return Math.floor(Math.random() * max);
+
+      case "canoutto":
+        return { ...state, number: state.number = 0 }
+        
+
   }
-  export default Reducers;
+  
+  return state;
+}
+
+export default counterReducers;
