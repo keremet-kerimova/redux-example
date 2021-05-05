da48a176dd3f9e470558c3f609e8adf60234bd88
Your Name <you@example.com>
2021-05-05 15:31:41 +0600

    chnage pri dobovlenie

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@
 // }
 // export  default App;
 import { useDispatch, useSelector } from "react-redux";
-import { add, remove } from "./redux/actions/todo";
+import { delay, remove } from "./redux/actions/todo";
 
 const App = () => {
   const items = useSelector(state => state);
@@ -37,7 +37,7 @@ const App = () => {
     event.preventDefault();
 
     const data = new FormData(event.target);
-    dispatch(add(data.get('new')));
+    dispatch(delay(data.get('new')));
   }
 
   const results = Object.keys(items).map(id => (
--- src/redux/actions/todo.js
+++ src/redux/actions/todo.js
@@ -1,5 +1,7 @@
 import { ADD_TODO, REMOVE_TODO } from "./types";
 
+import { ADD_TODO, REMOVE_TODO } from "./types";
+
 export const add = (text) => ({
   type: ADD_TODO,
   text: text
@@ -9,3 +11,11 @@ export const remove = (id) => ({
   type: REMOVE_TODO,
   id: id
 });
+
+export const delay = (text) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(add(text));
+    }, 3000);
+  }
+}
